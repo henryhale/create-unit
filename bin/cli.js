b2c5e87be8d869f1aa5c83a3c85bc63a9fe99940
Henry Hale <henryhale@users.noreply.github.com>
2024-01-17 18:30:37 +0300

    fix: write .gitignore file to output folder

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { copy } from "fs-extra";
+import { copy, outputFile } from "fs-extra";
 import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { log, error } from "node:console";
@@ -13,7 +13,7 @@ const src = join(dirname(fileURLToPath(import.meta.url)), "../starter");
 async function createProject() {
     try {
         await copy(src, dest);
-        await copy(`${src}/.gitignore`, `${dest}/.gitignore`);
+        await outputFile(`${dest}/.gitignore`, 'node_modules\ndist\n.unit');
         log(green(`[y] ${dest} created successfully! 🎉`));
     } catch (err) {
         error(red("[x] error creating project: " + err));
