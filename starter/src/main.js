8a7659e9da970cea92f4236ef7761f26e4374365
Henry Hale <henryharrison8117@gmail.com>
2024-01-18 04:00:02 +0300

    refactor: define states in top-level components that use it

--- starter/src/main.js
+++ starter/src/main.js
@@ -1,10 +1,7 @@
 import "./global.css";
 
 import Alpine from "alpinejs";
-import { defineState, defineTodoAppState } from "./data/state";
 
-defineState(Alpine);
-
-defineTodoAppState(Alpine);
+window.Alpine = Alpine;
 
 Alpine.start();
